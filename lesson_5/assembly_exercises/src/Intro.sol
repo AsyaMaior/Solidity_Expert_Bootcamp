d8f785cf97f659a7f95367a07d766f04fcd2b81c
asya <asyauniverse@gmail.com>
2024-03-12 23:36:00 +0300

    main: add assembly_exercises Intro

---
+++ lesson_5/assembly_exercises/src/Intro.sol
@@ -0,0 +1,14 @@
+//SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.4;
+
+contract Intro {
+    function intro() public pure returns (uint16) {
+
+        assembly {
+            let mol := 420
+            mstore(0x80, mol)
+            return(0x80, 0xff)
+        }
+    }
+}
---
+++ lesson_5/assembly_exercises/test/testIntro.t.sol
@@ -0,0 +1,20 @@
+//SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.4;
+
+import {Test, console} from "forge-std/Test.sol";
+// import {console} from "forge-std/console.sol";
+import {Intro} from "../src/Intro.sol";
+
+contract testIntro is Test {
+    Intro s_intro;
+    function setUp() external {
+        s_intro = new Intro();
+    }
+
+    function testReturnValue() external view {
+        uint16 result = s_intro.intro();
+        console.log("result: %s", result);
+        assertEq(result, 420);
+    }
+}
